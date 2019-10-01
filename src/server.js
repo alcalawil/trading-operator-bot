2d6e8a5d2510911c25e167f8ea31ff70c31ac17f
Wilfredo <alcalawilfre@gmail.com>
2019-10-01 13:22:12 -0300

    Add template.env

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 const server = http.createServer(app);
 
